94600af143006806e1033aef92cc36359f0326c9
zahidcserana <zahid1004048@gmail.com>
2019-10-15 09:12:33 +0600

    banner, footer

---
+++ resources/views/front/layout/footer.blade.php
@@ -0,0 +1,56 @@
+<footer class="footer_area">
+    <div class="container">
+        <div class="footer_widgets">
+            <div class="row">
+                <div class="col-lg-6 col-md-4 col-6">
+                    <aside class="f_widget f_about_widget">
+                        <a href="{{route('home_page')}}"><img src="{{asset('front/img/df-2.gif')}}" alt=""></a>
+
+                        <br>
+                        <ul>
+                            <li><a href="#"><i class="social_facebook"></i></a></li>
+                            <li><a href="#"><i class="social_twitter"></i></a></li>
+                            <li><a href="#"><i class="social_pinterest"></i></a></li>
+                            <li><a href="#"><i class="social_instagram"></i></a></li>
+                            <li><a href="#"><i class="social_youtube"></i></a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <aside class="f_widget link_widget f_info_widget">
+                        <div class="f_w_title">
+                            <h3>Information</h3>
+                        </div>
+                        <ul>
+                            <li><a href="javascript::void()">About us</a></li>
+                            <li><a href="javascript::void()">Terms & Conditions</a></li>
+                            <li><a href="javascript::void()">Help Center</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <aside class="f_widget link_widget f_service_widget">
+                        <div class="f_w_title">
+                            <h3>Customer Service</h3>
+                        </div>
+                        <ul>
+                            @foreach($categories as $row)
+                                <li><a href="{{ route('category_products',$row->id) }}">{{ $row->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </aside>
+                </div>
+            </div>
+        </div>
+        <div class="footer_copyright">
+            <h5>©
+                <script>document.write(new Date().getFullYear());</script>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
+                        href="https://colorlib.com" target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </h5>
+        </div>
+    </div>
+</footer>
